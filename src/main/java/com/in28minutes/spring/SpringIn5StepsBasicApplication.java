211e01db3ff383a492ccb34b831ad0bcdea296d2
Himanshu-Mishra-nit <himanshu.nit.mca@gmail.com>
2021-07-14 12:37:54 +0530

    Code orgnised in saparate package start learning scope in depth , used proxy in JdbcConnection

--- src/main/java/com/in28minutes/spring/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/SpringIn5StepsBasicApplication.java
@@ -1,20 +1,22 @@
-package com.in28minutes.spring.basics;
+package com.in28minutes.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.in28minutes.spring.basics.BinarySearchImpl;
+
 @SpringBootApplication
-public class SpringIn5StepsApplication {
+public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args); 
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args); 
 		BinarySearchImpl binary = applicationContext.getBean(BinarySearchImpl.class);
 		BinarySearchImpl binary1 = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binary.binarySearch(new int[] {12,4,3}, 3);
 		System.out.println(result);
 		System.out.println(binary);
-		System.out.println(binary1);
+		System.out.println(binary1 );
 		
 		
 		
