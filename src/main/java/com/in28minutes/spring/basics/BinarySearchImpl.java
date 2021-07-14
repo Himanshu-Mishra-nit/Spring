035aae0c4c646f11b977592b6b5dbb9a66aba40e
Himanshu-Mishra-nit <himanshu.nit.mca@gmail.com>
2021-07-14 11:34:58 +0530

    @Qualifier added for autowiring (3rd way)

--- src/main/java/com/in28minutes/spring/basics/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/BinarySearchImpl.java
@@ -1,20 +1,19 @@
 package com.in28minutes.spring.basics;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 	@Autowired
-	SortAlgorithm bubbleSortAlgorithm;
+	@Qualifier("quick")
+	SortAlgorithm sortAlgorithm;
 	
-	@Autowired
-	SortAlgorithm quickSortAlgorithm;
-	
-	
+
 	public int binarySearch(int []numbers, int numberToSearchFor) {
 		
-		numbers = quickSortAlgorithm.sort(numbers);
+		numbers = sortAlgorithm.sort(numbers);
 		//searching in array
 		//return result
 		return 3;
